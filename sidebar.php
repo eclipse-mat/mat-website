117b2f5948d094f844233848e29ec572a2617940
abuchen <abuchen>
2009-01-22 14:43:14 +0000

    update

--- sidebar.php
+++ sidebar.php
@@ -2,16 +2,6 @@
     $sidebar = <<<EOHTML
 
 		<div class="sideitem">
-			<h6>Related links</h6>
-			<ul>
-				<li><a href="http://wiki.eclipse.org/index.php/Modeling_Project">Modeling Project Wiki</a></li>
-				<li><a href="http://wiki.eclipse.org/index.php/Modeling_Corner">Modeling Corner</a></li>
-				<li><a href="http://wiki.eclipse.org/Ganymede">Ganymede</a>, <a href="http://wiki.eclipse.org/Europa">Europa</a>, <a href="http://www.eclipse.org/callisto">Callisto</a> - see 'Models and Model Development Tools' category.</li>
-				<li><a href="http://www.eclipse.org/modeling/project-info/team.php">Who`s Who in Modeling?</a></li>
-			</ul>
-		</div>
-
-		<div class="sideitem">
 			<h6>Contributors</h6>
 			<div class="modal">
 				<ul><li><a href="http://www.sap.com" target="_blank">SAP</a></li></ul>
@@ -20,17 +10,16 @@
 		
  		<div class="sideitem">
 			<h6>Incubation</h6>
-			<div class="modal">
-			<div align="center"><a href="/projects/what-is-incubation.php"><img
-			src="/images/egg-incubation.png" border="0" alt="Incubation" /></a></div>
+			<div align="center" width="100%"><a href="/projects/what-is-incubation.php"><img
+			src="/images/egg-incubation.png" border="0" alt="Incubation" align="center"/></a></div>
 			<div align="center">This project is in the
 				<a href="/projects/dev_process/validation-phase.php">Incubation Phase</a>
-			</div>
 			</div>
 		</div>
 		
 		<div class="sideitem">
 			<h6>Previous Talks</h6>
+			<div class="modal">
 			<ul>
 				<li><a href="http://www.java-forum-stuttgart.de">Java Forum Stuttgart</a>, July '08</li>
 				<li><a href="http://developers.sun.com/learning/javaoneonline/j1sessn.jsp?sessn=TS-5729&yr=2008&track=tools">JavaOne</a>, May '08</li>
@@ -38,13 +27,18 @@
 				<li><a href="http://entwickler.com/konferenzen/planer/show_details.php?konferenzid=59&sessionid=7774">JAX</a>, April '08</li>
 				<li><a href="http://www.eclipsecon.org/2008/index.php?page=sub/&id=321">EclipseCon</a>, March '08 (Slides)</li>
 			</ul>
+			</div>
 		</div>
 		
 		<div class="sideitem">
 			<h6>Newsgroup</h6>
-			<div align="left" style="padding:5px">Got questions? Need advice? Miss a feature?
-			<a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.technology.memory-analyzer">Newsgroup</a>
-			<a href="http://dev.eclipse.org/newslists/news.eclipse.technology.memory-analyzer/maillist.html">[archive]</a></div>
+			<div class="modal">
+			<ul>
+				<li>Got questions? Need advice? Miss a feature?
+				    <a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.technology.memory-analyzer">Newsgroup</a>
+			        <a href="http://dev.eclipse.org/newslists/news.eclipse.technology.memory-analyzer/maillist.html">[archive]</a></li>
+			</ul>
+			</div>
 		</div>
 
 EOHTML
