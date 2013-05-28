ec6d45f9a336658a6a6c40d41a008701cb465e34
Andrew Johnson <andrew_johnson@uk.ibm.com>
2013-05-28 13:44:14 +0100

    Fix links for talks

--- sidebar.php
+++ sidebar.php
@@ -15,14 +15,16 @@
 			<h6>Previous Talks</h6>
 			<div class="modal">
 			<ul>
-				<li><a href="http://www.eclipsecon.org/summiteurope2010/sessions/?page=sessions&amp;id=1764">Eclipse Summit Europe</a> Ludwigsburg, 4 November, '10</li>
+				<li><a href="http://www.slideshare.net/AJohnson1/extending-eclipse-memory-analyzer>Eclipse Summit Europe</a> Ludwigsburg, 4 November, '10</li>
 				<li><a href="http://javasymposium.techtarget.com/html/tools_tech.html#AJohnsonMemory">TheServerSide Java Symposium - Europe</a>, Prague, October '09</li>
-				<li><a href="http://developers.sun.com/learning/javaoneonline/j1sessn.jsp?sessn=TS-4118&amp;yr=2009&amp;track=javase">JavaOne</a>, June '09</li>
+				<li><a href="http://www.oracle.com/technetwork/java/j1sessn-jsp-156291.html">JavaOne</a>, San Francisco, June '09</li>
+				<li><a href="http://www.slideshare.net/guest62fd60c/eclipse-memory-analyzer-presentation">Eclipse Summit Europe</a> Ludwigsburg, 20 November, '08</li>
 				<li><a href="http://www.java-forum-stuttgart.de">Java Forum Stuttgart</a>, July '08</li>
-				<li><a href="http://developers.sun.com/learning/javaoneonline/j1sessn.jsp?sessn=TS-5729&amp;yr=2008&amp;track=tools">JavaOne</a>, May '08</li>
+				<li><a href="http://www.oracle.com/technetwork/java/j1sessn-jsp-156539.html">JavaOne</a>, San Francisco, May '08</li>
 				<li><a href="http://www.andrena.de/Entwicklertag/2008/Vortraege/Memory-Analyzer.html">Andrena Entwicklertag</a>, May '08</li>
 				<li><a href="http://entwickler.com/konferenzen/planer/show_details.php?konferenzid=59&amp;sessionid=7774">JAX</a>, April '08</li>
 				<li><a href="http://www.eclipsecon.org/2008/index.php?page=sub/&amp;id=321">EclipseCon</a>, March '08 (Slides)</li>
+				<li><a href="http://www.sdn.sap.com/irj/scn/go/portal/prtroot/docs/library/uuid/00ca7f0d-8ee6-2910-5d82-fc3e8dd25300">JavaOne</a>, San Francisco, May '07</li>
 			</ul>
 			</div>
 		</div>
