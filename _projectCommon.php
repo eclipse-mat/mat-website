a4caa4b727c98d5492e1a6cab3b8cea8810094e1
Krum Tsvetkov <krum.tsvetkov@sap.com>
2016-11-08 10:49:39 +0100

    Rearrange the menus on the left side
    Signed-off-by: Krum Tsvetkov <krum.tsvetkov@sap.com>

--- _projectCommon.php
+++ _projectCommon.php
@@ -16,28 +16,23 @@
 	$Menu->addMenuItem("Developers", "/mat/developers", "_self");
 
     $Nav->setLinkList(null);
-	$Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=tools.mat", "_self", 1);
 
 	$Nav->addNavSeparator("Memory Analyzer", "/mat/");
-	
+
+	$Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=tools.mat", "_self", 1);
     $Nav->addCustomNav("Screenshots", "/mat/about/screenshots.php", "_self", 2);
     $Nav->addCustomNav("Getting Started", "http://wiki.eclipse.org/index.php/MemoryAnalyzer", "_self", 2);
-    $Nav->addCustomNav("Documentation", "http://help.eclipse.org/topic/org.eclipse.mat.ui.help/welcome.html", "_self", 2);
-    $Nav->addCustomNav("FAQ (Wiki)", "http://wiki.eclipse.org/index.php/MemoryAnalyzer/FAQ", "_self", 2);
-    $Nav->addCustomNav("Forum", "http://www.eclipse.org/forums/eclipse.memory-analyzer", "_self", 2);
-    $Nav->addCustomNav("Blog", "http://dev.eclipse.org/blogs/memoryanalyzer", "_self", 2);
-    $Nav->addCustomNav("Unresolved bugs and enhancements", "https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced;order=Importance;bug_status=UNCONFIRMED;bug_status=NEW;bug_status=ASSIGNED;bug_status=REOPENED;product=MAT", 2);
-    $Nav->addCustomNav("Report Bug", "https://bugs.eclipse.org/bugs/enter_bug.cgi?product=MAT", "_self", 2);
+    $Nav->addCustomNav("Documentation", "/mat/documentation/", "_self", 2);
+    $Nav->addCustomNav("Blog", "http://memoryanalyzer.blogspot.in/", "_self", 2);
+    $Nav->addCustomNav("Get Help", "/mat/support/", 2);
 	
     $Nav->addNavSeparator("Downloads", "/mat/downloads.php");
     $Nav->addCustomNav("Releases", "/mat/downloads.php", "_self", 2);
     $Nav->addCustomNav("Nightly Builds", "/mat/snapshotBuilds.php", "_self", 2);
     
-    $Nav->addNavSeparator("Contributors", "http://wiki.eclipse.org/index.php?title=MemoryAnalyzer/Contributor_Reference");
-    $Nav->addCustomNav("Contributor Reference", "http://wiki.eclipse.org/index.php?title=MemoryAnalyzer/Contributor_Reference", "_self", 2);
+    $Nav->addNavSeparator("Developers", "http://wiki.eclipse.org/index.php?title=MemoryAnalyzer/Contributor_Reference");
     $Nav->addCustomNav("Mailing Lists", "https://dev.eclipse.org/mailman/listinfo/mat-dev", "_self", 2);
     $Nav->addCustomNav("View Git", "https://git.eclipse.org/c/mat/org.eclipse.mat.git", "_self", 2);
-    $Nav->addCustomNav("Project Plan", "https://projects.eclipse.org/projects/tools.mat/releases/", "_self", 2);
 
     $App->Promotion = TRUE;
 ?>
