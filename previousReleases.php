43d074c8757b159b5df11e1b65bbf064640a54d4
Krum Tsvetkov <krum.tsvetkov@sap.com>
2018-09-25 15:20:23 +0200

    Update download pages to 1.8.1
    Change-Id: Ia103b3061c34fb68e2ae2688aa4e021ac1b29696
    Signed-off-by: Krum Tsvetkov <krum.tsvetkov@sap.com>

--- previousReleases.php
+++ previousReleases.php
@@ -19,6 +19,41 @@
 		<a href="/birt">BIRT Chart Engine</a> (Version 2.3.0 or greater).</p>
 
 		<div class="homeitem3col">
+			<h3><a name="1_8_0"></a>Memory Analyzer 1.8.0 Release</h3>
+			<ul>
+			    <li>
+                    <b>Version</b>: 1.8.0.20180604 |  <b>Date</b>: 27 June 2018  |  <b>Type</b>: Released
+                    <ul>
+                        <li>
+                            <b>Update Site</b>: <a href="http://download.eclipse.org/mat/1.8/update-site/">http://download.eclipse.org/mat/1.8/update-site/</a>
+                        </li>
+                        <li>
+                            <b>Archived Update Site</b>: <a href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/MemoryAnalyzer-1.8.0.201806041349.zip">MemoryAnalyzer-1.7.0.201706130745.zip</a>
+                        </li>
+                        <li>
+		                    <b>Stand-alone Eclipse RCP Applications</b><br/>
+		                    
+		                    <a title="Windows RCP" href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-win32.win32.x86.zip"><img src="/mat/home/icon-save.gif" alt=""/></a>
+		                            &nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-win32.win32.x86.zip">Windows (x86)</a><br/>
+		                    <a title="Windows (x86_64) RCP" href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-win32.win32.x86_64.zip"><img src="/mat/home/icon-save.gif" alt=""/></a>
+		                                     &nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-win32.win32.x86_64.zip">Windows (x86_64)</a><br/>
+		                    <a title="Mac OSX (Mac/Cocoa/x86_64) RCP" href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-macosx.cocoa.x86_64.zip"><img src="/mat/home/icon-save.gif" alt=""/></a>
+		                                               &nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-macosx.cocoa.x86_64.zip">Mac OSX (Mac/Cocoa/x86_64)</a><br/>
+		                    <a title="Linux (x86/GTK+) RCP" href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-linux.gtk.x86.zip"><img src="/mat/home/icon-save.gif" alt=""/></a>
+		                                     &nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-linux.gtk.x86.zip">Linux (x86/GTK+)</a><br/>
+		                    <a title="Linux (x86_64/GTK+) RCP" href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-linux.gtk.x86_64.zip"><img src="/mat/home/icon-save.gif" alt=""/></a>
+		                                        &nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-linux.gtk.x86_64.zip">Linux (x86_64/GTK+)</a><br/>
+		                    <a title="Linux (PPC64/GTK+) RCP" href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-linux.gtk.ppc64.zip"><img src="/mat/home/icon-save.gif" alt=""/></a>
+		                                       &nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-linux.gtk.ppc64.zip">Linux (PPC64/GTK+)</a><br/>
+		                    <a title="Linux (PPC64le/GTK+) RCP" href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-linux.gtk.ppc64le.zip"><img src="/mat/home/icon-save.gif" alt=""/></a>
+		                                         &nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/mat/1.8/rcp/MemoryAnalyzer-1.8.0.20180604-linux.gtk.ppc64le.zip">Linux (PPC64le/GTK+)</a><br/>
+		                </li>
+                    </ul>
+                </li>
+			</ul>
+		</div>
+
+		<div class="homeitem3col">
 			<h3><a name="1_7_0"></a>Memory Analyzer 1.7.0 Release</h3>
 			<ul>
 			    <li>
